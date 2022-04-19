fcc85c6155a064d2ae859c47e2d3f77353381950
María Soledad <castiglia.ms@gmail.com>
2022-04-19 17:32:24 -0300

    Componente about

--- mi-portfolio/src/app/app.module.ts
+++ mi-portfolio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BotonComponent } from './components/boton/boton.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BotonComponent
+    BotonComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
